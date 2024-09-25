be9ed7c37444a64ccd6591e48584f2df66b304f5
TrungChienn81 <2254810012@vaa.edu.vn>
2024-09-25 07:59:06 +0700

    chien source 2 api
    api

--- routes/addMovie.js
+++ routes/addMovie.js
@@ -16,11 +16,51 @@ router.post('/fetch-movie', async (req, res) => {
         }
         const responseData = await fetch(url, options)
         const result = await responseData.json()
-        //console.log("Result", result)
-        res.json(result)
+        if (result.results.length === 0) {
+            return res.status(404).json({ error: 'No movies found with the given search term' });
+        }
+
+        res.render('addMovieList', { movieList: result.results })
+        // res.json(result)
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch movie details' });
+    }
+})
+
+router.get("/addMovie/:movieId", async (req, res) => {
+    const movieId = req.params.movieId;
+    //res.json(movieId);
+
+    try {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: process.env.TMDB_AUTH_KEY
+            }
+        };
+
+        const responseData = await fetch(url, options)
+        const movieDetails = await responseData.json()
+
+        const watchProviderUrl = `https://api.themoviedb.org/3/movie/${movieId}/watch/providers`;
+        const watchProvidersResponse = await fetch(watchProviderUrl, options)
+        const watchProvidersResult = await watchProvidersResponse.json()
+        const watchProviders = Object.keys(watchProvidersResult.results).filter((country) => country === "US").map((country) => {
+            const countryData = watchProvidersResult.results[country];
+            return {
+                country,
+                providerName: countryData.flatrate ? countryData.flatrate[0]?.provider_name : countryData.flatrate[0]?.provider_name
+            }
+        })
+        
+        movieDetails.watchProviders = watchProviders
+        res.json(movieDetails)
+
+    } catch (error) {
+
     }
 })
 module.exports = router;
