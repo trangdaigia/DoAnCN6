60229eabf22c88b36da9d059a2fee12feac9ba88
TrungChienn81 <2254810012@vaa.edu.vn>
2024-10-04 22:54:18 +0700

    1
    1

--- routes/addMovie.js
+++ routes/addMovie.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const router = express.Router()
-const Movie = require('../models/Movie');
+const Movie = require('../models/movie');
 
 
 
--- routes/updateMovie.js
+++ routes/updateMovie.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Movie = require('../models/Movie'); 
+const Movie = require('../models/movie'); 
 
 router.get('/edit-movie-list', async (req, res) => {
     try {
